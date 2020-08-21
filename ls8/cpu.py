f26a4cfda44b883b1d37deebfaeb9d3a5bef480f
caleb <baregrcj20@hotmail.com>
2020-08-21 08:44:41 -0700

    program can run sctest :)

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -9,6 +9,7 @@ class CPU:
         self.registers = [0] * 8 # 8 general purpose registers
         self.registers[7] = 0xf4 # this is where the stack will start
         
+        self.fl = 0b00000000 # initalize the flag LGE
         self.pc = 0 # program counter
         self.running = False
 
@@ -23,6 +24,17 @@ class CPU:
         self.branchtable[0b01010000] = self.call
         self.branchtable[0b00010001] = self.ret
         self.branchtable[0b10100000] = self.add
+        self.branchtable[0b10100111] = self.cmp
+        self.branchtable[0b01010101] = self.jeq
+        self.branchtable[0b01010110] = self.jne
+        self.branchtable[0b01010100] = self.jmp
+
+
+    # hlp_plz
+    def _move_pc(self, ir):
+        mv_amt = (self.memory[ir] >> 6) + 1
+        self.pc += mv_amt
+
     # Operation handlers
     def hlt(self, ir):
         self.running = False
@@ -70,6 +82,40 @@ class CPU:
         # store the result in reg1
         self.registers[self.memory[ir + 1]] = self.registers[self.memory[ir + 1]] + self.registers[self.memory[ir + 2]] 
 
+    def cmp(self, ir):
+        reg1_num = self.memory[ir + 1]
+        reg2_num = self.memory[ir + 2]
+        if self.registers[reg1_num] == self.registers[reg2_num]:
+            self.fl = 0b00000001
+        elif self.registers[reg1_num] < self.registers[reg2_num]:
+            self.fl = 0b00000010
+        else:
+            self.fl = 0b00000100
+
+    def jeq(self, ir):
+        # (0b00000000 >> 2) & 1 == L
+        # (0b00000000 >> 1) & 1 == G 
+        # (0b00000000) & 1 == E
+        
+        # if the equal flag is true than
+        # jump to the address stored at the given register
+        if (self.fl >> 2) & 1 == 1:
+            reg_num = self.memory[ir + 1]
+            self.pc = self.registers[reg_num]
+        else:
+            self._move_pc(ir)
+
+    def jne(self, ir):
+        if self.fl & 1 == 0:
+            reg_num = self.memory[ir + 1]
+            self.pc = self.registers[reg_num]
+        else:
+            self._move_pc(ir)
+
+    def jmp(self, ir): # jump to address at given register
+        reg_num = self.memory[ir + 1]
+        self.pc = self.registers[reg_num]
+
     def ram_read(self, MAR): # MAR ( Memory Address Register)
         return self.registers[MAR]
     
@@ -102,34 +148,34 @@ class CPU:
             print(f"could not open {filename}")
             sys.exit(2)
         
-    def alu(self, op, reg_a, reg_b):
-        """ALU operations."""
+    # def alu(self, op, reg_a, reg_b):
+    #     """ALU operations."""
 
-        if op == "ADD":  
-            self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
-        else:
-            raise Exception("Unsupported ALU operation")
+    #     if op == "ADD":  
+    #         self.reg[reg_a] += self.reg[reg_b]
+    #     #elif op == "SUB": etc
+    #     else:
+    #         raise Exception("Unsupported ALU operation")
 
-    def trace(self):
-        """
-        Handy function to print out the CPU state. You might want to call this
-        from run() if you need help debugging.
-        """
+    # def trace(self):
+    #     """
+    #     Handy function to print out the CPU state. You might want to call this
+    #     from run() if you need help debugging.
+    #     """
 
-        print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
-            #self.fl,
-            #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
-        ), end='')
+    #     print(f"TRACE: %02X | %02X %02X %02X |" % (
+    #         self.pc,
+    #         #self.fl,
+    #         #self.ie,
+    #         self.ram_read(self.pc),
+    #         self.ram_read(self.pc + 1),
+    #         self.ram_read(self.pc + 2)
+    #     ), end='')
 
-        for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+    #     for i in range(8):
+    #         print(" %02X" % self.reg[i], end='')
 
-        print()
+    #     print()
 
     def run(self):
         """Run the CPU."""
@@ -139,6 +185,8 @@ class CPU:
 
             self.branchtable[self.memory[ir]](ir)
 
+            # if not (self.memory[ir] >> 4) & 1:
+            #     mv_amt = (self.memory[ir] >> 6) + 1
+            #     self.pc += mv_amt
             if not (self.memory[ir] >> 4) & 1:
-                mv_amt = (self.memory[ir] >> 6) + 1
-                self.pc += mv_amt
+                self._move_pc(ir)
